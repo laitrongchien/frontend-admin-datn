10b9ac15e274dc3322de7e564f9087272ddb0dd9
laitrongchien <laichien2002@gmail.com>
2024-04-15 01:17:06 +0700

    Update in home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { useAppSelector } from "@/store/hooks";
 import { FaFlagCheckered, FaMotorcycle } from "react-icons/fa";
 import { MdReceiptLong } from "react-icons/md";
@@ -10,12 +11,15 @@ import { tourService } from "@/services/api/tour";
 import { motorbikeService } from "@/services/api/motorbike";
 import { bookingService } from "@/services/api/booking";
 import { rentalService } from "@/services/api/rental";
+import TableData from "@/components/table/TableData";
+import Loading from "@/components/Loading";
 
 const Dashboard = () => {
   const [tours, setTours] = useState([]);
   const [motorbikes, setMotorbikes] = useState([]);
   const [bookings, setBookings] = useState([]);
   const [rentals, setRentals] = useState([]);
+  const [loading, setLoading] = useState(false);
   const { user } = useAppSelector((state) => state.auth);
   const router = useRouter();
 
@@ -28,10 +32,12 @@ const Dashboard = () => {
   useEffect(() => {
     const getData = async () => {
       try {
+        setLoading(true);
         const toursRes = await tourService.getAllTours();
         const motorbikesRes = await motorbikeService.getAllMotorbikes();
         const bookingsRes = await bookingService.getAllTourBookings();
         const rentalsRes = await rentalService.getAllMotorbikeRentals();
+        setLoading(false);
         setTours(toursRes.data.tours);
         setMotorbikes(motorbikesRes.data.motorbikes);
         setBookings(bookingsRes.data);
@@ -42,6 +48,135 @@ const Dashboard = () => {
     };
     getData();
   }, []);
+
+  const bookingColumns = [
+    {
+      name: "Mã đơn đặt tour",
+      selector: (row: any) => row?._id,
+    },
+    {
+      name: "Tên người đặt",
+      selector: (row: any) => row?.user.name,
+    },
+    {
+      name: "Email",
+      selector: (row: any) => row?.user.email,
+      wrap: true,
+    },
+    {
+      name: "Thanh toán",
+      cell: (row: any) => (
+        <h1
+          className={`
+                ${
+                  row?.paymentType === "payAll"
+                    ? "text-success"
+                    : "text-pending"
+                } font-semibold
+              `}
+        >
+          {row?.paymentType === "payAll"
+            ? "Đã thanh toán toàn bộ"
+            : "Thanh toán trước 20%"}
+        </h1>
+      ),
+      wrap: true,
+    },
+    {
+      name: "Trạng thái",
+      cell: (row: any) => (
+        <h1
+          className={`
+              ${
+                row?.status === "completed"
+                  ? "text-success"
+                  : row?.status === "started"
+                  ? "text-primary"
+                  : row?.status === "not-started"
+                  ? "text-error"
+                  : "text-pending"
+              } font-semibold
+            `}
+        >
+          {row?.status === "completed"
+            ? "Đã hoàn thành"
+            : row?.status === "started"
+            ? "Đã tham gia"
+            : row?.status === "not-started"
+            ? "Không tham gia"
+            : "Chưa khởi hành"}
+        </h1>
+      ),
+      wrap: true,
+    },
+  ];
+
+  const rentalColumns = [
+    {
+      name: "Mã đơn thuê xe",
+      selector: (row: any) => row?._id,
+    },
+    {
+      name: "Tên người thuê",
+      selector: (row: any) => row?.user.name,
+    },
+    {
+      name: "Email",
+      selector: (row: any) => row?.user.email,
+      wrap: true,
+    },
+    {
+      name: "Thanh toán",
+      cell: (row: any) => (
+        <h1
+          className={`
+            ${
+              row?.paymentType === "payAll" ? "text-success" : "text-pending"
+            } font-semibold
+          `}
+        >
+          {row?.paymentType === "payAll"
+            ? "Đã thanh toán toàn bộ"
+            : "Thanh toán trước 20%"}
+        </h1>
+      ),
+      wrap: true,
+    },
+    {
+      name: "Trạng thái",
+      cell: (row: any) => (
+        <h1
+          className={`
+            ${
+              row?.status === "returned"
+                ? "text-success"
+                : row?.status === "received"
+                ? "text-primary"
+                : row?.status === "not-received"
+                ? "text-error"
+                : "text-pending"
+            } font-semibold
+          `}
+        >
+          {row?.status === "returned"
+            ? "Đã trả xe"
+            : row?.status === "received"
+            ? "Đã nhận xe"
+            : row?.status === "not-received"
+            ? "Không nhận xe"
+            : "Chờ nhận xe"}
+        </h1>
+      ),
+      wrap: true,
+    },
+  ];
+
+  if (loading)
+    return (
+      <div className="h-[calc(100vh-80px-36px)]">
+        <Loading />
+      </div>
+    );
 
   return (
     <div>
@@ -101,6 +236,23 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      {/* <div className="p-2">
+        <h1 className="text-lg">Đơn đặt tour gần đây</h1>
+        <div className="border border-gray-200 rounded-lg mt-4">
+          <TableData columns={bookingColumns} data={bookings} />
+        </div>
+      </div> */}
+      <div className="p-2 mt-4">
+        <div className="flex-between">
+          <h1 className="text-lg">Đơn thuê xe gần đây</h1>
+          <Link href={"/bills/motor-rental"} className="text-sm">
+            Xem tất cả
+          </Link>
+        </div>
+        <div className="border border-gray-200 rounded-lg mt-4">
+          <TableData columns={rentalColumns} data={rentals.slice(0, 5)} />
         </div>
       </div>
     </div>
